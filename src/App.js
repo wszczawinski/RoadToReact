97c225d57ac6ac6950c9039774cab29ef37fa982
wszczawinski <w.k.szczawinski@gmail.com>
2020-02-09 22:02:26 +0100

    HOC for button with loading

--- src/App.js
+++ src/App.js
@@ -138,18 +138,23 @@ class App extends Component {
         }
 
         <div className="interactions">
-          {isLoading
-            ? <Loading/>
-            : <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
-                More
-              </Button>
-          }
+          <ButtonWithLoading isLoading={isLoading} onClick={() => this.fetchSearchTopStories(searchKey, page + 1)} >
+            More
+          </ButtonWithLoading>
         </div>
         
       </div>
     );
   }
 }
+ 
+const withLoading = (Component) => ({isLoading, ...rest}) =>
+  isLoading
+    ? <Loading />
+    : <Component { ...rest } />
+
+const ButtonWithLoading = withLoading(Button);
+
 
 const Loading = () => 
   <div>Loading...</div>
